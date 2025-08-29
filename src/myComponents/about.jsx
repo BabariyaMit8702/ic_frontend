724efce1605574a262946bc1c68bcf8e479eca8a
BabariyaMit8702 <mit944996@gmail.com>
2025-08-29 11:20:26 +0530

    install rtk,route dependency and tailwind for start project

---
+++ src/myComponents/about.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='bg-amber-600'>ABout</div>
+  )
+}
---
+++ src/myComponents/first.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../App.css'
+
+export const First = () => {
+  return (
+    <div className='text-amber-300 bg-blue-900'>First</div>
+  )
+}
