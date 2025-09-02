458c92803b536904b7f850eb80acf8f468244c38
BabariyaMit8702 <mit944996@gmail.com>
2025-09-02 14:28:48 +0530

    add mycircle text insted of instagram logo

--- src/myComponents/home.jsx
+++ src/myComponents/home.jsx
@@ -20,32 +20,38 @@ export const Home = () => {
     <div className="min-h-screen bg-gray-900 text-white flex flex-col md:flex-row">
 
       {/* ---------- Navbar ---------- */}
-      <nav className="fixed top-0 left-0 right-0 md:static bg-gray-900 border-b border-gray-700 flex md:flex-col items-center justify-between md:justify-start p-2 md:w-[80px] lg:w-[250px] z-50">
-        {/* Desktop Name + Icon */}
-        <div className="hidden lg:flex items-center transform scale-150 m-7 space-x-2 p-2">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
-            alt="#@"
-            className="h-8 invert"
-          />
-        </div>
+     <nav className="
+  fixed 
+  bottom-0 left-0 right-0 
+  lg:top-0 lg:left-0 lg:h-screen 
+  bg-gray-950 border-t lg:border-t-0 lg:border-r border-gray-700 
+  flex justify-around items-center 
+  lg:flex-col lg:items-start lg:justify-start 
+  p-2 lg:w-[250px] 
+  z-50
+">
+  {/* Desktop Logo */}
+  <div className="hidden lg:flex lg:text-white items-center transform scale-150 ml-10 mt-7 space-x-2 p-2">
+  <h3 className='logo_nav'>MyCircle</h3>
+  </div>
 
-        {/* Icons */}
-        <div className="flex justify-around md:flex-col md:space-y-4 w-full lg:w-auto mt-2 md:mt-10">
-          {/* Home Icon */}
-          <svg className="h-6 w-6 md:h-8 md:w-8 cursor-pointer" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M3 9.75L12 3l9 6.75V21a.75.75 0 01-.75.75H3.75A.75.75 0 013 21V9.75z"/>
-          </svg>
-          {/* Search Icon */}
-          <svg className="h-6 w-6 md:h-8 md:w-8 cursor-pointer" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M12 2a10 10 0 100 20 10 10 0 000-20z"/>
-          </svg>
-          {/* Reels Icon */}
-          <svg className="h-6 w-6 md:h-8 md:w-8 cursor-pointer" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M3 3h18v18H3V3z"/>
-          </svg>
-        </div>
-      </nav>
+  {/* Icons */}
+  <div className="flex justify-around w-full lg:w-auto lg:flex-col lg:space-y-6 mt-0 lg:mt-10">
+    {/* Home Icon */}
+    <svg className="h-7 w-7 cursor-pointer" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M3 9.75L12 3l9 6.75V21a.75.75 0 01-.75.75H3.75A.75.75 0 013 21V9.75z"/>
+    </svg>
+    {/* Search Icon */}
+    <svg className="h-7 w-7 cursor-pointer" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M12 2a10 10 0 100 20 10 10 0 000-20z"/>
+    </svg>
+    {/* Reels Icon */}
+    <svg className="h-7 w-7 cursor-pointer" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M3 3h18v18H3V3z"/>
+    </svg>
+  </div>
+</nav>
+
 
       {/* ---------- Home Feed ---------- */}
       <main className="flex-1 flex flex-col items-center justify-start pt-16 md:pt-2 p-4 md:p-6 lg:p-8 overflow-y-auto">
@@ -60,7 +66,7 @@ export const Home = () => {
       </main>
 
       {/* ---------- Mobile Bottom Navbar ---------- */}
-      <nav className="fixed bottom-0 left-0 right-0 bg-gray-900 border-t border-gray-700 flex justify-around p-2 md:hidden z-50">
+      {/* <nav className="fixed bottom-0 left-0 right-0 bg-gray-900 border-t border-gray-700 flex justify-around p-2 md:hidden z-50">
         <svg className="h-6 w-6 cursor-pointer" fill="currentColor" viewBox="0 0 24 24">
           <path d="M3 9.75L12 3l9 6.75V21a.75.75 0 01-.75.75H3.75A.75.75 0 013 21V9.75z"/>
         </svg>
@@ -70,7 +76,7 @@ export const Home = () => {
         <svg className="h-6 w-6 cursor-pointer" fill="currentColor" viewBox="0 0 24 24">
           <path d="M3 3h18v18H3V3z"/>
         </svg>
-      </nav>
+      </nav> */}
     </div>
     </>
   )
